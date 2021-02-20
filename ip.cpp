a2ce73c6dec0393b73111d9945dc5581b5e111fe
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-19 21:40:18 -0700

    ip and running sum

---
+++ ip.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+  string defangIPaddr(string address) { return address; }
+};
+
+int main(int argc, char const *argv[]) { return 0; }
--- rsum.cpp
+++ rsum.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution
 {
 public:
-  vector<int> runningSum(vector<int> &nums) {}
+  vector<int> runningSum(vector<int> &nums)
+  {
+
+    int sum = 0;
+
+    for (int i = 0; i < nums.size(); ++i)
+    {
+      sum += nums[i];
+      nums[i] = sum;
+    }
+
+    return nums;
+  }
 };
 
-int main(int argc, char const *argv[]) { return 0; }
+int main(int argc, char const *argv[])
+{
+
+  vector<int> nums = {1, 2, 3, 4};
+
+  Solution *soln = new Solution();
+
+  nums = soln->runningSum(nums);
+
+  for (int i = 0; i < nums.size(); ++i)
+  {
+    cout << nums[i] << " ";
+  }
+
+  cout << endl;
+
+  return 0;
+}
