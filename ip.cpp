efab527b31bbf964ffb9ece82c23c8b86ba5c97d
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-20 06:46:27 -0700

    updated ip

--- ip.cpp
+++ ip.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 class Solution
 {
 public:
-  string defangIPaddr(string address) { return address; }
+  string defangIPaddr(string address)
+  {
+
+    string defang = "";
+
+    for (int i = 0; i < address.size(); ++i)
+    {
+      if (address[i] == '.')
+      {
+        defang += "[.]";
+      }
+      else
+      {
+        defang += address[i];
+      }
+    }
+
+    return defang;
+  }
 };
 
-int main(int argc, char const *argv[]) { return 0; }
+int main(int argc, char const *argv[])
+{
+
+  Solution *obj = new Solution();
+  string address = "10.10.10.10";
+
+  address = obj->defangIPaddr(address);
+
+  cout << address << endl;
+  return 0;
+}
