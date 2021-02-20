cd5fc9c5b4affe2db9b51e8bced94fd88c2e062a
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-20 07:35:45 -0700

    solved problems

---
+++ shuffle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> shuffle(vector<int> &nums, int n) { return nums; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  return 0;
+}
