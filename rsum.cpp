fbb11fca8a16f6a1bdba61423a3c4fb74633eb57
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-19 18:10:18 -0700

    rsum

---
+++ rsum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> runningSum(vector<int> &nums) {}
+};
+
+int main(int argc, char const *argv[]) { return 0; }
