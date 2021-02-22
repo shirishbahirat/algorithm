567aeb77e4b046a5ec1d1e04d2b6177932866595
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-21 22:17:20 -0700

    solved problems

---
+++ get_small.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> smallerNumbersThanCurrent(vector<int> &nums) { return nums; }
+};
+
+void swap(vector<int>::iterator x, vector<int>::iterator y)
+{
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+void quick_sort(vector<int> &vec, vector<int>::iterator left,
+                vector<int>::iterator right)
+{
+
+  auto size = distance(left, right);
+  if (size <= 1)
+  {
+    return;
+  }
+  auto pivot = next(right, -1);
+  if (size == 2 && *pivot < *left)
+  {
+    iter_swap(left, pivot);
+  }
+  auto wall = left;
+  auto curr = left;
+
+  while (curr != right)
+  {
+    if (*curr < *pivot)
+    {
+      iter_swap(wall, curr);
+      wall++;
+    }
+    curr++;
+  }
+
+  swap(pivot, wall);
+  quick_sort(vec, left, wall);
+  quick_sort(vec, wall + 1, right);
+}
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  vector<int> nums = {8, 1, 2, 0, 3, 5};
+
+  quick_sort(nums, nums.begin(), nums.end());
+
+  for (int i = 0; i < nums.size(); ++i)
+  {
+    cout << nums.at(i) << " ";
+  }
+
+  cout << endl;
+
+  nums = obj->smallerNumbersThanCurrent(nums);
+
+  return 0;
+}
