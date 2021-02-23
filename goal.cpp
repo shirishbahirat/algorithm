229c2cdad37c3bbac2aeaf13a02bad9b0f07ecc0
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-22 18:25:57 -0700

    solved problems

---
+++ goal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+  string interpret(string command) { return command; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  return 0;
+}
