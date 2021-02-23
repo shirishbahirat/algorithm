8e1aba8aee7223ff87820fbcf266ce1d62ceb986
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-22 20:27:28 -0700

    solved problems

--- goal.cpp
+++ goal.cpp
@@ -4,11 +4,43 @@ using namespace std;
 class Solution
 {
 public:
-  string interpret(string command) { return command; }
+  string interpret(string command)
+  {
+
+    string parsed;
+
+    for (int i = 0; i < command.size(); ++i)
+    {
+      if ((command[i] == '(') && (command[i + 1] == ')'))
+      {
+        parsed += 'o';
+        ++i;
+      }
+      else if ((command[i] == '(') && (command[i + 1] == 'a'))
+      {
+        parsed += 'a';
+        ++i;
+      }
+      else if ((command[i] == 'l') && (command[i + 1] == ')'))
+      {
+        parsed += 'l';
+        ++i;
+      }
+      else
+      {
+        parsed += command[i];
+      }
+    }
+
+    return parsed;
+  }
 };
 
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
+  string command = "G()(al)";
+  string parsed = obj->interpret(command);
+  cout << parsed << endl;
   return 0;
 }
