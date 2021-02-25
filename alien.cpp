fe9387607ad6a16bd90423286fe0a7954371d86e
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-24 19:34:35 -0700

    solved problems

---
+++ alien.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  bool isAlienSorted(vector<string> &words, string order) { return true; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  string order = "bb";
+  vector<string> words = {"aa", "bb"};
+  obj->isAlienSorted(words, order);
+  return 0;
+}
