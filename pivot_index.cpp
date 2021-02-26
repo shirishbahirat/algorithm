930ca9c195bef0d84889cfb9c441a1b6c85339ea
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-26 07:43:24 -0700

    solved problems

---
+++ pivot_index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  int pivotIndex(vector<int> &nums) {}
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  return 0;
+}
