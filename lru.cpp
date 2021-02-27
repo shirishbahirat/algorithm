7885e028957778ec6753b6e8171f8c49a5382b5f
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-27 10:54:47 -0700

    solved problems

---
+++ lru.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class LRUCache
+{
+public:
+  LRUCache(int capacity) : size(capacity) {}
+
+  int get(int key)
+  {
+
+    int idx = -1;
+
+    for (int i = 0; i < lru.size(); ++i)
+    {
+      if (lru[i] == key)
+        idx = i;
+    }
+
+    if (idx >= 0)
+    {
+      lru.erase(lru.begin() + idx);
+    }
+    else
+    {
+      return -1;
+    }
+
+    lru.push_back(key);
+
+    return cache[key];
+  }
+
+  void put(int key, int value)
+  {
+
+    int idx = -1;
+
+    for (int i = 0; i < lru.size(); ++i)
+    {
+      if (lru[i] == key)
+        idx = i;
+    }
+
+    if (idx >= 0)
+    {
+      cache[key] = value;
+    }
+    else
+    {
+      if (lru.size() > size)
+      {
+        return;
+      }
+      else
+      {
+        lru.push_back(key);
+        cache[key] = value;
+      }
+    }
+
+    for (int i = 0; i < lru.size(); ++i)
+    {
+      cout << lru[i] << " ";
+    }
+    cout << endl;
+  }
+
+private:
+  int size;
+  map<int, int> cache;
+  vector<int> lru;
+};
+
+int main(int argc, char const *argv[])
+{
+  LRUCache *obj = new LRUCache(2);
+  obj->get(2);
+  obj->put(2, 6);
+  cout << obj->get(1) << endl;
+  obj->put(1, 5);
+  obj->put(1, 2);
+  cout << obj->get(1) << endl;
+  cout << obj->get(2) << endl;
+
+  return 0;
+}
