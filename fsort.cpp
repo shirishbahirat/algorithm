db36c2349bca79c2e66c6e7f704ef91673fbf2ed
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-27 09:15:19 -0700

    solved problems

--- fsort.cpp
+++ fsort.cpp
@@ -10,11 +10,23 @@ public:
   vector<int> frequencySort(vector<int> &nums)
   {
     map<int, int> frequency;
-    map<int, vector<int>> ordered;
+    multimap<int, int> ordered;
 
     for (int i = 0; i < nums.size(); ++i)
     {
       frequency[nums[i]] += 1;
+    }
+
+    for (auto const &[key, value] : frequency)
+    {
+      ordered.insert(pair<int, int>(value, key));
+    }
+
+    int idx = 0;
+    for (auto const &[key, value] : ordered)
+    {
+      for (int i = 0; i < key; ++i)
+        nums[idx++] = value;
     }
 
     return nums;
@@ -24,7 +36,7 @@ public:
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
-  vector<int> nums = {4, 4, 1};
+  vector<int> nums = {4, 4, 1, 5, 5};
 
   nums = obj->frequencySort(nums);
 
