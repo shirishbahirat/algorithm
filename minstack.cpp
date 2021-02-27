aee4b53418c624545d260ca1e7743736a7acdceb
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-02-27 13:26:48 -0700

    solved problems

--- minstack.cpp
+++ minstack.cpp
@@ -8,23 +8,61 @@ class MinStack
 {
 public:
   /** initialize your data structure here. */
-  MinStack() : min(0x7FFFFFF) {}
+  MinStack() : min(0x7FFFFFFFFFFFFFF) {}
 
-  void push(int x) {}
+  void push(long int x)
+  {
+    stack.insert(stack.begin(), x);
+    if (x < min)
+      min = x;
+  }
 
-  void pop() {}
+  void pop()
+  {
 
-  int top() { return 0; }
+    bool get_min;
+
+    if (min == stack[0])
+    {
+      get_min = true;
+    }
+
+    stack.erase(stack.begin());
+
+    if (get_min)
+    {
+      min = 0x7FFFFFFFFFFFFFF;
+      for (int i = 0; i < stack.size(); ++i)
+      {
+        if (stack[i] < min)
+          min = stack[i];
+      }
+    }
+  }
+
+  int top() { return stack[0]; }
 
   int getMin() { return min; }
 
 private:
-  vector<int> stack;
-  int min;
+  vector<long int> stack;
+  long int min;
 };
 
 int main(int argc, char const *argv[])
 {
   MinStack *obj = new MinStack();
+
+  obj->push(-2);
+  obj->push(0);
+  obj->push(-3);
+
+  cout << obj->getMin() << endl;
+
+  obj->pop();
+
+  cout << "yop " << obj->top() << endl;
+  cout << obj->getMin() << endl;
+
   return 0;
 }
