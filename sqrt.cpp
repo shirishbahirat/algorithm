96ae67d1a93eadde7d8c4b1ea1f8c6fa1ed9f4cb
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-03-03 21:06:34 -0700

    solved problems

--- sqrt.cpp
+++ sqrt.cpp
@@ -6,14 +6,28 @@ using namespace std;
 class Solution
 {
 public:
-  int mySqrt(int x) { return 0; }
+  int mySqrt(int x)
+  {
+
+    if (x == 0)
+      return 0;
+
+    double error = 0.00001;
+    long int s = x;
+
+    while ((s - x / s) > error)
+    {
+      s = (s + x / s) / 2;
+    }
+    return s;
+  }
 };
 
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
 
-  cout << obj->mySqrt(8) << endl;
+  cout << obj->mySqrt(2147483647) << endl;
 
   return 0;
 }
