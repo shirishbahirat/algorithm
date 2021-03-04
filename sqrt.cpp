a63858f35c58f6b955b0ec0fd4f8fd6e26da8f15
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-03-03 20:52:09 -0700

    solved problems

---
+++ sqrt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int mySqrt(int x) { return 0; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+
+  cout << obj->mySqrt(8) << endl;
+
+  return 0;
+}
