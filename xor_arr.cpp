b1f705a4ac4b6244d19046565f0a25101b2071f1
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-03-05 22:36:02 -0700

    solved problems

--- xor_arr.cpp
+++ xor_arr.cpp
@@ -6,14 +6,25 @@ using namespace std;
 class Solution
 {
 public:
-  int xorOperation(int n, int start) { return 10; }
+  int xorOperation(int n, int start)
+  {
+
+    vector<int> nums(n, 0);
+
+    for (int i = 0; i < n; ++i)
+    {
+      nums[i] = start + (2 * i);
+    }
+
+    return 10;
+  }
 };
 
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
 
-  obj->xorOperation(2, 1);
+  obj->xorOperation(5, 1);
 
   return 0;
 }
