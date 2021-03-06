eb0cc9757a2f3d97b73cfdb667ba2a3f2453b4f4
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-03-05 21:41:22 -0700

    solved problems

---
+++ xor_arr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int xorOperation(int n, int start) { return 0; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  obj->xorOperation(2, 1);
+  return 0;
+}
