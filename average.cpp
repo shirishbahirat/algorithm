21df09fdcec98441ddd2f21364c920c7d954e225
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-03-13 08:29:19 -0700

    solved problems

---
+++ average.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int average(vector<int> &nums)
+  {
+
+    int avg = 0;
+
+    for (int x : nums)
+    {
+      avg += x;
+    }
+
+    avg /= nums.size();
+
+    return avg;
+  }
+};
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  vector<int> nums = {10, 11, 2, 56, 3, 9, 34};
+  cout << obj->average(nums) << endl;
+  return 0;
+}
