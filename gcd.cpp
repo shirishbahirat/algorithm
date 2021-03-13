dc01cae849cb928b06f88cd074a38d042adf76a1
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-03-12 21:36:26 -0700

    solved problems

---
+++ gcd.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int gcd(int a, int b)
+  {
+
+    while (a != b)
+    {
+      if (a > b)
+        a -= b;
+      else
+        b -= a;
+    }
+
+    return a;
+  }
+};
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+
+  cout << obj->gcd(10, 20) << endl;
+
+  return 0;
+}
