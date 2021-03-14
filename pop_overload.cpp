50e13fde981aa37213ebf3324dfd55006657a106
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-03-13 22:48:24 -0700

    solved problems

---
+++ pop_overload.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class complex
+{
+public:
+  complex(int a, int b) : real(a), imag(b) {}
+
+  int get_real() { return real; }
+  int get_imag() { return imag; }
+
+private:
+  int real;
+  int imag;
+};
+
+complex &operator+(complex &x, complex &y)
+{
+  int r = x.get_real() + y.get_real();
+  int i = x.get_imag() + y.get_imag();
+
+  complex *temp = new complex(r, i);
+
+  return *temp;
+}
+
+int main(int argc, char const *argv[])
+{
+  complex *x = new complex(5, 10);
+  complex *y = new complex(20, 25);
+
+  complex z = *x + *y;
+
+  cout << z.get_real() << " " << z.get_imag() << endl;
+
+  return 0;
+}
