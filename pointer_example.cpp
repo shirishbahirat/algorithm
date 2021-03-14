8f2d6effc9b53bb06835c6ab41ddadebf2d65d74
shirishbahirat@comcast.net <shirishbahirat@comcast.net>
2021-03-13 21:57:24 -0700

    solved problems

---
+++ pointer_example.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  int a = 10;
+  int *p = &a;
+  cout << a << endl;
+  cout << &a << endl;
+  cout << p << endl;
+  cout << &p << endl;
+  cout << *p << endl;
+  return 0;
+}
