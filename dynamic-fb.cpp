3173fde451597da1af12d2bcab6d35bc15b8e9e6
Shirish Bahirat <shirishbahirat@comcast.net>
2021-05-17 18:13:51 -0600

    solved problems

---
+++ dynamic-fb.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int fib(int number)
+  {
+    if (number == 0)
+      return 0;
+
+    if (number <= 2)
+      return 1;
+
+    return fib(number - 1) + fib(number - 2);
+  }
+};
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+
+  cout << obj->fib(0) << endl;
+  cout << obj->fib(1) << endl;
+  cout << obj->fib(2) << endl;
+  cout << obj->fib(3) << endl;
+  cout << obj->fib(4) << endl;
+  cout << obj->fib(5) << endl;
+  cout << obj->fib(6) << endl;
+  cout << obj->fib(7) << endl;
+  cout << obj->fib(8) << endl;
+  cout << obj->fib(9) << endl;
+
+  return 0;
+}
