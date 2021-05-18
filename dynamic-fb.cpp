61578e32f782e93e89ddc67b04aec69374efcf06
Shirish Bahirat <shirishbahirat@comcast.net>
2021-05-17 18:23:24 -0600

    solved problems

--- dynamic-fb.cpp
+++ dynamic-fb.cpp
@@ -8,13 +8,7 @@ class Solution
 public:
   vector<int> v;
 
-  Solution() : v(20, -1)
-  {
-    /*for (int i = 0; i < 20; ++i)
-    {
-      v.push_back(-1);
-    }*/
-  }
+  Solution() : v(20, -1) {}
 
   ~Solution() {}
 
