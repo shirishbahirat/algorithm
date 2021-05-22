df29a6f9ec356f878bbd86c9f517dddcea1dfc06
Shirish Bahirat <shirishbahirat@comcast.net>
2021-05-22 11:11:46 -0600

    solved problems

---
+++ fpointer.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void (*f)(int);
+
+void test(int c) { cout << c << endl; }
+
+class Solution
+{
+public:
+  Solution() {}
+
+  static void (*f)(int);
+
+  int pivotIndex(vector<int> &nums) { return 0; }
+
+  static void test(int c) { cout << c + 100 << endl; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  f = test;
+
+  f(100);
+  f(111);
+
+  f = obj->test;
+
+  f(100);
+
+  return 0;
+}
