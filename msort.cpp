5a72088413df9ac3d3f32d8d430b9a0c006ec8fb
Shirish Bahirat <shirishbahirat@comcast.net>
2021-05-26 22:16:49 -0600

    solved problems

---
+++ msort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  Solution() : arr{10, 2, 4, 6, 3, 1}, len(5) {}
+
+  int split(int lo, int hi, int a[]) { return 0; }
+
+  void msort(int lo, int hi)
+  {
+
+    int mid = (hi - lo) / 2;
+
+    return;
+  }
+
+  int len;
+  int arr[];
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+
+  obj->msort(0, 5);
+  return 0;
+}
