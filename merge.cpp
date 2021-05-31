5ca5f38e6616872f5d84247e6d31b13c2c64dd24
Shirish Bahirat <shirishbahirat@comcast.net>
2021-05-31 10:03:06 -0600

    solved problems

---
+++ merge.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  Solution() : a{2, 4, 6, 7}, b{0, 1, 3, 5, 8} {}
+
+  ~Solution() {}
+
+  void merge(void)
+  {
+
+    int alen = sizeof(a) / sizeof(a[0]);
+    int blen = sizeof(b) / sizeof(b[0]);
+    int clan = sizeof(c) / sizeof(c[0]);
+
+    return;
+  }
+
+  int a[4], b[5], c[9];
+};
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+
+  obj->merge();
+
+  return 0;
+}
