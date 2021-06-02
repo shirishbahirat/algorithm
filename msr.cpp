66afa38f18ca641ec8ce6c2265e8f0ca60d3e58c
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-01 21:39:28 -0600

    solved problems

--- msr.cpp
+++ msr.cpp
@@ -8,14 +8,57 @@ class Solution
 public:
   Solution() : arr{6, 5, 4, 1, 2, 8, 3, 9} {}
 
-  void msort(int arr[], int lo, int mid, int hi) { return; }
+  void msort(int arr[], int lo, int mid, int hi)
+  {
+
+    int ln = mid - lo + 1;
+    int lm = hi - mid;
+
+    int left[ln], right[lm];
+
+    for (int i = 0; i < ln; ++i)
+    {
+      left[i] = arr[lo + i];
+    }
+
+    for (int i = 0; i < lm; ++i)
+    {
+      right[i] = arr[mid + i + 1];
+    }
+
+    int i = 0, j = 0, k = lo;
+
+    while ((i < ln) && (j < lm))
+    {
+      if (left[i] <= right[j])
+      {
+        arr[k++] = left[i++];
+      }
+      else
+      {
+        arr[k++] = right[j++];
+      }
+    }
+
+    while (i < ln)
+    {
+      arr[k++] = left[i++];
+    }
+
+    while (j < lm)
+    {
+      arr[k++] = right[j++];
+    }
+
+    return;
+  }
 
   void merge(int arr[], int lo, int hi)
   {
 
     if (hi > lo)
     {
-      int mid = lo + (hi - lo) / 2;
+      int mid = lo + ((hi - lo) / 2);
       merge(arr, lo, mid);
       merge(arr, mid + 1, hi);
       msort(arr, lo, mid, hi);
@@ -26,6 +69,13 @@ public:
   {
 
     merge(arr, 0, 7);
+
+    for (int i = 0; i <= 7; ++i)
+    {
+      cout << arr[i] << " ";
+    }
+
+    cout << endl;
 
     return;
   }
