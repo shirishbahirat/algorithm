6fa1b744b6c7dd58061db2df49b9b52d369c94d8
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-04 22:46:22 -0600

    solved problems

--- mrgs.cpp
+++ mrgs.cpp
@@ -2,4 +2,11 @@
 
 using namespace std;
 
-int main(int argc, const char *argv[]) { return 0; }
+int main(int argc, const char *argv[])
+{
+
+  int arr[] = {6, 4, 3, 2, 1, 7, 9, 8};
+  int hi = sizeof(arr) / sizeof(arr[0]);
+
+  return 0;
+}
