b4f6f654abe66411a564a93a6a3471fc24bccd4f
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-05 21:33:34 -0600

    solved problems

---
+++ quesrt.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int arr[], int idx, int idy)
+{
+  if (idx != idy)
+  {
+    int temp = arr[idx];
+    arr[idx] = arr[idy];
+    arr[idy] = temp;
+  }
+}
+
+int partition(int arr[], int lo, int hi)
+{
+
+  int i = lo, j = hi;
+
+  int pivot = arr[lo];
+
+  while (j > i)
+  {
+    while (arr[i] <= pivot)
+    {
+      if (i < hi)
+      {
+        ++i;
+      }
+    }
+
+    while (arr[j] > pivot)
+    {
+      if (j > lo)
+      {
+        --j;
+      }
+    }
+
+    if (i > j)
+    {
+      swap(arr, i, j);
+    }
+  }
+  swap(arr, lo, j);
+
+  return j;
+}
+
+void quick_sort(int arr[], int lo, int hi)
+{
+  if (hi > lo)
+  {
+    int mid = partition(arr, lo, hi);
+    quick_sort(arr, lo, mid);
+    quick_sort(arr, mid + 1, hi);
+  }
+
+  return;
+}
+
+int main(int argc, const char *argv[])
+{
+
+  int arr[] = {9, 12, 23, 4, 2, 18, 23};
+  int hi = (sizeof(arr) / sizeof(arr[0])) - 1;
+
+  quick_sort(arr, 0, hi);
+
+  for (int a : arr)
+    cout << a << " ";
+  cout << endl;
+
+  return 0;
+}
