13d547e920b1d131cea782262bf74674de18ed2a
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-07 21:08:55 -0600

    solved problems

--- heap.cpp
+++ heap.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 
-void trickle_up(int arr[], int position) {}
+using namespace std;
+
+void swap(int arr[], int idx, int idy)
+{
+  if (idx != idy)
+  {
+    int temp = arr[idx];
+    arr[idx] = arr[idy];
+    arr[idy] = temp;
+  }
+}
+
+void trickle_up(int arr[], int position)
+{
+
+  if (!position)
+    return;
+
+  int parent = (position - 1) / 2;
+
+  if (arr[position] > arr[parent])
+  {
+    swap(arr, position, parent);
+    trickle_up(arr, parent);
+  }
+}
 
 int main(int argc, const char *argv[])
 {
 
   int arr[] = {4, 2, 3, 6};
+
+  trickle_up(arr, 3);
+
+  for (int a : arr)
+    cout << a << " ";
+  cout << endl;
 
   return 0;
 }
