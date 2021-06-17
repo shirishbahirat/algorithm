99be34235470b61dc1caf900cf3b8b14ee6ae695
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-17 07:22:22 -0600

    solved problems

--- min_moves.cpp
+++ min_moves.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -6,13 +7,34 @@ using namespace std;
 class Solution
 {
 public:
-    int pivotIndex(vector<int> &nums)
+  int numRescueBoats(vector<int> &people, int limit)
+  {
+
+    sort(people.begin(), people.end());
+    int start = 0;
+    int end = people.size();
+    int boats = 0;
+
+    while (start == end)
     {
-         return 0;
+      if (start == limit)
+      {
+        boats++;
+        start++;
+      }
+      if (end == limit)
+      {
+        boats++;
+        end--;
+      }
     }
+
+    return boats;
+  }
 };
+
 int main(int argc, char const *argv[])
 {
-    Solution *obj = new Solution();
-    return 0;
-  }
+  Solution *obj = new Solution();
+  return 0;
+}
