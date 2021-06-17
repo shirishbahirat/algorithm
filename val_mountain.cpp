33154c7cd6c60e674ff20dc9c7ae69371aa86e27
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-17 10:35:37 -0600

    solved problems

---
+++ val_mountain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  bool validMountainArray(vector<int> &arr) { return true; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  return 0;
+}
