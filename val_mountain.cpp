e2d5ccb2afe156c4a6a46284d3b3b49775d5744b
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-17 13:28:30 -0600

    solved problems

--- val_mountain.cpp
+++ val_mountain.cpp
@@ -6,7 +6,28 @@ using namespace std;
 class Solution
 {
 public:
-  bool validMountainArray(vector<int> &arr) { return true; }
+  bool validMountainArray(vector<int> &arr)
+  {
+    int i = 0;
+
+    while ((arr[i] < arr[i - 1]) && (i < arr.size()))
+    {
+      i++;
+    }
+
+    if ((i == 1) || (i == arr.size()))
+      return false;
+
+    while ((arr[i] > arr[i - 1]) && (i < arr.size()))
+    {
+      i++;
+    }
+
+    if (i == arr.size())
+      return true;
+
+    return false;
+  }
 };
 
 int main(int argc, char const *argv[])
