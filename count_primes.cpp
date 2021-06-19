22f352abb388cfe79623170b85f45ed264457526
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-18 22:11:26 -0600

    solved problems

---
+++ count_primes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int countPrimes(int n) { return 0; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+
+  cout << obj->countPrimes(10) << endl;
+  return 0;
+}
