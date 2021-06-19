cd0969ff9c03f8d9ad48f61ee8223e1c9146efb3
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-18 21:29:13 -0600

    solved problems

---
+++ first_bad_version.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int firstBadVersion(int n) { return 0; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  return 0;
+}
