023da81b2799e687360afa57a08cde99644ce918
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-18 21:58:26 -0600

    solved problems

--- first_bad_version.cpp
+++ first_bad_version.cpp
@@ -3,10 +3,30 @@
 
 using namespace std;
 
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
 class Solution
 {
 public:
-  int firstBadVersion(int n) { return 0; }
+  int firstBadVersion(int n)
+  {
+
+    int left = 1;
+    int right = n;
+
+    while (left < right)
+    {
+      int mid = left + ((right - left) / 2);
+
+      if (!isBadVersion(mid))
+        left = mid + 1;
+      else
+        right = mid;
+    }
+
+    return left;
+  }
 };
 
 int main(int argc, char const *argv[])
