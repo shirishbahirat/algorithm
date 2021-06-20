0a0bcb6e00a4e56b3208322916f206d85d9f7bbc
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-19 19:56:37 -0600

    solved problems

--- robot_oigin.cpp
+++ robot_oigin.cpp
@@ -6,15 +6,46 @@ using namespace std;
 class Solution
 {
 public:
-  bool judgeCircle(string moves) { return true; }
+  bool judgeCircle(string moves)
+  {
+
+    int idx = 0;
+    int idy = 0;
+
+    for (int i = 0; i < moves.size(); ++i)
+    {
+      switch (moves[i])
+      {
+      case 'U':
+        idy++;
+        break;
+      case 'D':
+        idy--;
+        break;
+      case 'L':
+        idx--;
+        break;
+      case 'R':
+        idx++;
+        break;
+      default:
+        break;
+      }
+    }
+
+    if ((idx == 0) and (idy == 0))
+      return true;
+
+    return false;
+  }
 };
 
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
-  string moves = "test";
+  string moves = "LL";
 
-  obj->judgeCircle(moves);
+  cout << obj->judgeCircle(moves) << endl;
 
   return 0;
 }
