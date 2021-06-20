7b4e51325c3660b36cbffddb34b31987196c045f
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-20 14:19:48 -0600

    solved problems

---
+++ group_anagrams.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  vector<vector<string>> groupAnagrams(vector<string> &strs)
+  {
+    vector<vector<string>> ans;
+    vector<string> temp = strs;
+    map<string, vector<int>> groups;
+
+    for (int i = 0; i < strs.size(); i++)
+    {
+      sort(temp[i].begin(), temp[i].end());
+      cout << temp[i] << endl;
+      groups[temp[i]].push_back(i);
+    }
+
+    int idx = 0;
+
+    for (const auto &[key, value] : groups)
+    {
+      vector<int> grp = groups[key];
+
+      cout << key << endl;
+
+      ans.push_back({});
+
+      for (int i = 0; i < grp.size(); i++)
+      {
+        ans[idx].push_back(strs[grp[i]]);
+      }
+
+      idx++;
+    }
+
+    return ans;
+  }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+
+  vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+  obj->groupAnagrams(strs);
+  return 0;
+}
