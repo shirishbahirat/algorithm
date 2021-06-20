c17e0b43771f41d1f1543b939ce8bc091b24c9fe
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-19 18:18:38 -0600

    solved problems

--- count_primes.cpp
+++ count_primes.cpp
@@ -6,7 +6,41 @@ using namespace std;
 class Solution
 {
 public:
-  int countPrimes(int n) { return 0; }
+  int countPrimes(int n)
+  {
+
+    if (n < 2)
+      return 0;
+
+    vector<bool> primes(n, true);
+
+    primes[0] = false;
+    primes[1] = false;
+
+    int idx = 2;
+    int idy = 2;
+    int ans = 0;
+
+    while ((idx * idx) < n)
+    {
+      while ((idx * idy) < n)
+      {
+        int num = idx * idy;
+        primes[num] = false;
+        idy++;
+      }
+      idx++;
+      idy = idx;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+      if (primes[i] == true)
+        ans++;
+    }
+
+    return ans;
+  }
 };
 
 int main(int argc, char const *argv[])
