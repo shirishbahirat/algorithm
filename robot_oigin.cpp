9794cfba69ccac0d53220c08ae1e431d0be3e90f
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-19 18:44:17 -0600

    solved problems

---
+++ robot_oigin.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  bool judgeCircle(string moves) { return true; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  string moves = "test";
+
+  obj->judgeCircle(moves);
+
+  return 0;
+}
--- single_number.cpp
+++ single_number.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <vector>
 
 using namespace std;
@@ -6,7 +7,26 @@ using namespace std;
 class Solution
 {
 public:
-  int singleNumber(vector<int> &nums) { return 0; }
+  int singleNumber(vector<int> &nums)
+  {
+
+    map<int, int> count;
+
+    for (int i = 0; i < nums.size(); ++i)
+    {
+      int idx = nums[i];
+      count[idx]++;
+    }
+
+    for (int i = 0; i < nums.size(); ++i)
+    {
+      int idx = nums[i];
+      if (count[idx] == 1)
+        return idx;
+    }
+
+    return 0;
+  }
 };
 
 int main(int argc, char const *argv[])
