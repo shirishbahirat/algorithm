71926b92e41acfaef8aabd91b52ab3fdc8afe119
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-21 07:43:14 -0600

    solved problems

--- four_sum_two.cpp
+++ four_sum_two.cpp
@@ -3,19 +3,32 @@
 
 using namespace std;
 
-class Solution
+class Solution class Solution
 {
 public:
   int fourSumCount(vector<int> &nums1, vector<int> &nums2, vector<int> &nums3,
                    vector<int> &nums4)
   {
 
-    return 0;
+    int cnt = 0;
+    unordered_map<int, int> m;
+    for (int a : nums1)
+      for (int b : nums2)
+        ++m[a + b];
+    for (int c : nums3)
+      for (int d : nums4)
+      {
+        auto it = m.find(-(c + d));
+        if (it != end(m))
+          cnt += it->second;
+      }
+    return cnt;
   }
 };
 
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
+
   return 0;
 }
