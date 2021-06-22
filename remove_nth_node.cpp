48a4f7c2044d11a69b16e1c42e1a5b317531579f
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-21 19:52:30 -0600

    solved problems

--- remove_nth_node.cpp
+++ remove_nth_node.cpp
@@ -3,16 +3,48 @@
 
 using namespace std;
 
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution
 {
 public:
-    int pivotIndex(vector<int> &nums)
+  ListNode *removeNthFromEnd(ListNode *head, int n)
+  {
+    int val = 1;
+
+    ListNode *temp = nullptr;
+    ListNode *list = head;
+
+    while (head)
     {
-         return 0;
+      val++;
+
+      if (val == n)
+      {
+        temp = head;
+      }
+
+      head = head->next;
+
+      if (val > n)
+      {
+        temp = temp->next;
+      }
     }
+
+    return list;
+  }
 };
+
 int main(int argc, char const *argv[])
 {
-    Solution *obj = new Solution();
-    return 0;
-  }
+  Solution *obj = new Solution();
+  return 0;
+}
