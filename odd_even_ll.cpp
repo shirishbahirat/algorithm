99e59aaea5357b782f3a8204f680daf1f90300dd
Shirish Bahirat <shirishbahirat@comcast.net>
2021-06-25 11:31:47 -0600

    solved problems

--- odd_even_ll.cpp
+++ odd_even_ll.cpp
@@ -22,7 +22,7 @@ int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
 
-  ListNode *head = new ListNode(1011);
+  ListNode *head = new ListNode(1012);
 
   obj->oddEvenList(head);
 
