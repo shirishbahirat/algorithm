02ef2a64e62be2cbf7ba4e47877fd236dea0883e
Shirish Bahirat <shirishbahirat@comcast.net>
2021-07-04 07:34:03 -0600

    solved problems

--- sorts.cpp
+++ sorts.cpp
@@ -3,4 +3,32 @@
 
 using namespace std;
 
+int len = 10;
+int aarr[] = {10, 9, 8, 3, 6, 5, 4, 7, 2, 1};
+
+void swap(int arr[], int x, int y, int len)
+{
+
+  if (x < 0 || y < 0)
+    return;
+  if (x > len || y > len)
+    return;
+
+  if (x != y)
+  {
+    int temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+  }
+}
+
+void print(int arr[], int len)
+{
+  for (int i = 0; i <= len; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
 int main(int argc, char const *argv[]) { return 0; }
