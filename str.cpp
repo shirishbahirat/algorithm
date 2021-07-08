1646a00338d2999b784d2be0947ae0cf3133a8a2
Shirish Bahirat <shirishbahirat@comcast.net>
2021-07-07 22:23:13 -0600

    solved problems

--- str.cpp
+++ str.cpp
@@ -2,4 +2,24 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[]) { return 0; }
+struct node
+{
+  int manager[2];
+  int idx;
+  int data;
+};
+
+int main(int argc, char const *argv[])
+{
+
+  node nd = {
+      .data = 1000,
+      .manager = {4, 5},
+      .idx = 10,
+  };
+
+  cout << nd.manager[0] << " " << nd.manager[1] << endl;
+  cout << nd.idx << " " << nd.data << endl;
+
+  return 0;
+}
