d6e9db0129f96c63f2063620f7945cb1eb84354b
Shirish Bahirat <shirishbahirat@comcast.net>
2021-07-08 22:07:25 -0600

    solved problems

--- lambda.cpp
+++ lambda.cpp
@@ -16,9 +16,11 @@ int main(int argc, char const *argv[])
   int w = 11;
   auto m = [](int x) { return x + 20; };
   auto p = [w]() { return (w + 20); };
+
+  void (*f)(int) = [](int i) { std::cout << i; };
+
   largest(m(20));
   largest(p());
-  largest((int)([w]() { return (w + 20); }));
 
   cout << m(10) << endl;
 
