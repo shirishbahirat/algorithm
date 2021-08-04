ceea08ead75d34d0ca84f5f82a693f7edf686a7d
Shirish Bahirat <shirishbahirat@comcast.net>
2021-08-03 18:53:59 -0600

    solved problems

--- api.cpp
+++ api.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+using namespace std;
 
 typedef int (*function)(int);
 
 struct api
 {
-
   function send;
 };
 
-int main(int argc, char const *argv[]) { return 0; }
+int send(int data)
+{
+
+  cout << "from api " << data << endl;
+
+  return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+
+  api vm = {.function = send;
+};
+
+return 0;
+}
