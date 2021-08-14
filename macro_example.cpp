12e672fbfb3466c8a81a9d9229415fba2785fefc
Shirish Bahirat <shirishbahirat@comcast.net>
2021-08-14 10:09:45 -0600

    solved problems

--- macro_example.cpp
+++ macro_example.cpp
@@ -2,4 +2,12 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[]) { return 0; }
+#define ADD(a, b, c) a##b##c
+
+int main(int argc, char const *argv[])
+{
+
+  cout << ADD("a", "b", "c") << endl;
+
+  return 0;
+}
