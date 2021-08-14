31cbe4728b1f54c0d7a5d294dc4e0c0749b5d944
Shirish Bahirat <shirishbahirat@comcast.net>
2021-08-14 10:30:03 -0600

    solved problems

--- macro_example.cpp
+++ macro_example.cpp
@@ -17,7 +17,7 @@ struct test_st
   }
 
 #define ADDK(a, b, c) test_st testt = {.dataa = a, .datab = b, .datac = c}
-#define ADDX(a, b, c) test_st testt = INIT_STRUCTURE(testt, a, b, c)
+#define ADDX(a, b, c) test_st testx = INIT_STRUCTURE(testx, a, b, c)
 
 int main(int argc, char const *argv[])
 {
