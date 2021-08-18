97123be3057ffaabdc13beca06a912e9966c2475
Shirish Bahirat <shirishbahirat@comcast.net>
2021-08-17 19:12:44 -0600

    solved problems

--- fileio.cpp
+++ fileio.cpp
@@ -1,4 +1,20 @@
 #include <cstdio>
 #include <iostream>
 
-int main(int argc, char const *argv[]) { return 0; }
+int main(int argc, char const *argv[])
+{
+
+  FILE *fid = fopen("test_textfile.txt", "r");
+
+  double trainsample;
+  if (fid)
+  {
+    while (!feof(fid))
+    {
+      fscanf(fid, "%lf", &trainsample);
+      printf("%lf\n", trainsample);
+    }
+  }
+
+  return 0;
+}
