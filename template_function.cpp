8063844f3da213e976d205f34255b36d221aeadc
Shirish Bahirat <shirishbahirat@comcast.net>
2021-08-20 19:53:59 -0600

    solved problems

--- template_function.cpp
+++ template_function.cpp
@@ -4,7 +4,7 @@ using namespace std;
 template <class t> t maxim(t a, t b) { return a > b ? a : b; }
 int main()
 {
-  cout << maxim(15, 14) << endl;
+  cout << maxim(16, 14) << endl;
   cout << maxim(2.4, 1.4) << endl;
   cout << maxim(2.3f, 4.6f) << endl;
   return 0;
