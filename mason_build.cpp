93c984b4ae34167c14f50dc33368dad2e9cefa43
Shirish Bahirat <shirishbahirat@comcast.net>
2021-08-23 21:49:30 -0600

    solved problems

--- mason_build.cpp
+++ mason_build.cpp
@@ -7,5 +7,12 @@ int arr[] = {[10] = 10};
 int main(int argc, const char *argv[])
 {
   cout << "Hello there" << endl;
+
+  for (int i = 0; i < 10; i++)
+  {
+    cout << arr[i] << " " << endl;
+  }
+  cout << endl;
+
   return 0;
 }
