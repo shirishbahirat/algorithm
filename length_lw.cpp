f674f07887457cf41d76c625a3048a69a70ad0de
Shirish Bahirat <shirishbahirat@comcast.net>
2021-08-27 21:24:39 -0600

    solved problems

--- length_lw.cpp
+++ length_lw.cpp
@@ -10,8 +10,22 @@ public:
   {
 
     int n = s.size();
+    int last = 0;
+    int first = 0;
 
     cout << n << " " << s[n - 1] << endl;
+
+    while (n--)
+    {
+      if ((last == 0) && (s[n] = !" "))
+        last = n;
+
+      if ((first == 0) && (n[n] < last) && (s[n] == " "))
+      {
+        first = n;
+        return (last - first);
+      }
+    }
 
     return 0;
   }
