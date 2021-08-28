ab71dc423e9accfe3bfd59f5ea74ea6c97b389b8
Shirish Bahirat <shirishbahirat@comcast.net>
2021-08-28 11:04:07 -0600

    solved problems

--- prod_sign.cpp
+++ prod_sign.cpp
@@ -6,13 +6,15 @@ using namespace std;
 class Solution
 {
 public:
-    int pivotIndex(vector<int> &nums)
-    {
-         return 0;
-    }
+  int pivotIndex(vector<int> &nums) { return 0; }
 };
 int main(int argc, char const *argv[])
 {
-    Solution *obj = new Solution();
-    return 0;
-  }
+  Solution *obj = new Solution();
+
+  vector<int> nums = {-1, -2, -3, -4, 3, 2, 1};
+
+  cout << obj->pivotIndex(nums) << endl;
+
+  return 0;
+}
