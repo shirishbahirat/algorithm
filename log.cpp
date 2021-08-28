eee728e4c4c35650804d5fefaf219c50696e5311
Shirish Bahirat <shirishbahirat@comcast.net>
2021-08-28 08:11:30 -0600

    solved problems

--- log.cpp
+++ log.cpp
@@ -17,10 +17,8 @@ int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
 
-  vector<string> input = [
-    "dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig",
-    "let3 art zero"
-  ];
+  vector<string> input = {"dig1 8 1 5 1", "let1 art can", "dig2 3 6",
+                          "let2 own kit dig", "let3 art zero"};
 
   return 0;
 }
