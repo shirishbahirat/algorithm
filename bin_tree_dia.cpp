07f202f78062c86e3b535dad4467ac8b1634e36a
Shirish Bahirat <shirishbahirat@comcast.net>
2021-09-02 19:21:49 -0600

    solved problems

---
+++ bin_tree_dia.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+  int diameterOfBinaryTree(TreeNode *root) { return 0; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  return 0;
+}
