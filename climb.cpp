b5dea087e511d5537535dd84b3f6c23e2b50e7d2
Shirish Bahirat <shirishbahirat@comcast.net>
2021-09-02 19:15:20 -0600

    solved problems

--- climb.cpp
+++ climb.cpp
@@ -6,7 +6,25 @@ using namespace std;
 class Solution
 {
 public:
-  int climbStairs(int n) { return 0; }
+  int climbStairs(int n)
+  {
+
+    if (n < 3)
+      return n;
+
+    int current = 0;
+    int prev = 2;
+    int prev_prev = 1;
+
+    for (int i = 3; i < n; i++)
+    {
+      current = prev_prev + prev;
+      prev_prev = prev;
+      prev = current;
+    }
+
+    return current;
+  }
 };
 int main(int argc, char const *argv[])
 {
