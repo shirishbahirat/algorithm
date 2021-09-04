e406e7796895bce25202b4f88037ef9872451a7b
Shirish Bahirat <shirishbahirat@comcast.net>
2021-09-03 21:06:11 -0600

    solved problems

--- hello.c
+++ hello.c
@@ -5,12 +5,15 @@ typedef char *DEBUG_MSG;
 
 DEBUG_MSG DATA = "Hello world\n";
 
-#define DEBUG_MSG "%s",
+#define DEBUG_MSG
+
+#define NO_MSG
 
 int main(int argc, char **argv)
 {
 
   printf(DEBUG_MSG "test message\n");
+  printf(NO_MSG "test no message\n");
 
   return 0;
 }
