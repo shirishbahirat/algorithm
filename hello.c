f91515346363606312ecbb97d99d1aca77fbfcef
Shirish Bahirat <shirishbahirat@comcast.net>
2021-09-03 20:48:22 -0600

    solved problems

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef char DEBUG_MSG;
+
+DEBUG_MSG DATA[] = "Hello world\n";
+
+int main(int argc, char **argv)
+{
+
+  printf("TEST\n");
+
+  return 0;
+}
