7d831cc910ae2fdb6f5ce547bf655e4fe5914293
Shirish Bahirat <shirishbahirat@comcast.net>
2021-09-14 20:44:26 -0600

    solved problems

--- pull_idx.cpp
+++ pull_idx.cpp
@@ -98,7 +98,7 @@ int main(int argc, char const *argv[])
 
   cout << n->tag << endl;
   pifo = remove_node(pifo);
-  cout << pifo->data << endl;
+  cout << pifo->next->data << endl;
 
   return 0;
 }
