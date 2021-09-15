f04761de150ccd9751005df9e4afc8872094a621
Shirish Bahirat <shirishbahirat@comcast.net>
2021-09-14 19:35:54 -0600

    solved problems

---
+++ pull_idx.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct node
+{
+  int data;
+  int tag;
+  node *next;
+
+  node() : data(int d), tag(int t);
+  next(nullptr);
+};
+
+void insert_node() {}
+
+int main(int argc, char const *argv[]) { return 0; }
