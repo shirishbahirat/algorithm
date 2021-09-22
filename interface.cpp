7141b95248e285eefd4664f1217cc2c50f24afe0
Shirish Bahirat <shirishbahirat@comcast.net>
2021-09-21 20:08:23 -0600

    solved problems

--- interface.cpp
+++ interface.cpp
@@ -7,16 +7,16 @@ struct consumer_interface;
 
 struct producer_interface
 {
-  virtual void transmit(int cmd) = 0;
-  virtual bool ready() = 0;
-  virtual void connect(struct *consumer_interface) = 0;
+  virtual void transmit(int cmd) {}
+  virtual bool ready(void) {}
+  // virtual void connect(struct *consumer_interface) {}
 };
 
 struct consumer_interface
 {
-  virtual void receive(int cmd) = 0;
-  virtual bool ready() = 0;
-  virtual void connect(struct *producer_interface) = 0;
+  virtual void receive(int cmd) {}
+  virtual bool ready(void) {}
+  // virtual void connect(struct *producer_interface) {}
 };
 
 int main(int argc, char const *argv[]) { return 0; }
