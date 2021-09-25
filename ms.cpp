199de159a042fc1ff4de0b97ef962ef3e0c81825
Shirish Bahirat <shirishbahirat@comcast.net>
2021-09-25 12:40:40 -0600

    solved problems

---
+++ ms.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void merge_sort(int arr[], int lo, int hi)
+{
+  if (hi > lo)
+  {
+    int mid = lo + ((hi - lo) / 2);
+    merge_sort(arr, lo, mid);
+    merge_sort(arr, mid + 1, hi);
+  }
+}
+
+int main(int argc, char const *argv[])
+{
+
+  int arr[] = {9, 4, 3, 7, 6, 5, 10, 1};
+
+  return 0;
+}
