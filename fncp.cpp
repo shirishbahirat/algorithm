c09c93c0cda0b6a854e2f0f8cccdbd1afda622de
Shirish Bahirat <shirishbahirat@comcast.net>
2021-09-29 19:40:16 -0600

    solved problems

--- fncp.cpp
+++ fncp.cpp
@@ -25,13 +25,13 @@ int main(int argc, char const *argv[])
   };
 
   car tesla = {
-      .manu_date.day = 10,
-      .manu_date.month = 2,
-      .manu_date.year = 2020,
-      .purc_date.day = 22,
-      .purc_date.month = 12,
-      .purc_date.year = 2021,
-      .purc_price = 22000,
+      .manu_date.day = 6,
+      .manu_date.month = 9,
+      .manu_date.year = 2021,
+      .purc_date.day = 12,
+      .purc_date.month = 3,
+      .purc_date.year = 2020,
+      .purc_price = 65000,
   };
 
   return 0;
