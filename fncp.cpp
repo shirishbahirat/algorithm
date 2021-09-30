5c5409258e1142d7ec1744ca01b754282fcf6f80
Shirish Bahirat <shirishbahirat@comcast.net>
2021-09-29 19:38:24 -0600

    solved problems

--- fncp.cpp
+++ fncp.cpp
@@ -7,16 +7,23 @@ typedef struct
 
 typedef struct
 {
-  date manu_date, pur_date;
-  double pur_price;
+  date manu_date, purc_date;
+  double purc_price;
 } car;
 
 int main(int argc, char const *argv[])
 {
 
   car honda = {
-      .manu_date.day = 10, .manu_date.month = 2, .manu_date.year = 2021};
-  car tesla;
+      .manu_date.day = 10,
+      .manu_date.month = 2,
+      .manu_date.year = 2020,
+      .purc_date.day = 22,
+      .purc_date.month = 12,
+      .purc_date.year = 2021,
+  };
+};
+car tesla;
 
-  return 0;
+return 0;
 }
