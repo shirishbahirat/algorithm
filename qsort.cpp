50577c65f2cddcbde7b370b9e96a37909103b099
Shirish Bahirat <shirishbahirat@comcast.net>
2021-10-05 07:48:56 -0600

    solved problems

--- qsort.cpp
+++ qsort.cpp
@@ -60,7 +60,7 @@ void quick_sort(int arr[], int lo, int hi)
 
 int main(int argc, char const *argv[])
 {
-  int arr[] = {3, 4, 7, 2, 10, 1, 5, 8, 9, 11, 6};
+  int arr[] = {3, 4, 7, 2, 10, 0, 1, 5, 8, 9, 11, 6};
   int size = (sizeof(arr) / sizeof(arr[0])) - 1;
 
   quick_sort(arr, 0, size);
