6d0924f663754c1c8b8ce7a0a1b4206f13f69532
Shirish Bahirat <shirishbahirat@comcast.net>
2021-10-09 09:31:08 -0600

    solved problems

--- trickle_dn.cpp
+++ trickle_dn.cpp
@@ -12,23 +12,23 @@ void swap(int arr[], int idx, int idy)
   }
 }
 
-void trickle_down(int arr[], int idx)
+void trickle_down(int arr[], int idx, int len)
 {
 
   int largest = idx;
   int left = 2 * idx + 1;
   int right = 2 * idx + 2;
 
-  if (arr[largest] > arr[left])
+  if ((arr[largest] > arr[left]) && (left < len))
     largest = left;
 
-  if (arr[largest] > arr[right])
+  if ((arr[largest] > arr[right]) && (right < len))
     largest = right;
 
   if (largest != idx)
   {
     swap(arr, idx, largest);
-    trickle_down(arr, largest);
+    trickle_down(arr, largest, len);
   }
 }
 
@@ -37,7 +37,7 @@ int main(int argc, char const *argv[])
   int arr[] = {10, 4, 6, 8, 7, 8, 9};
   int len = sizeof(arr) / sizeof(arr[0]);
 
-  trickle_down(arr, 0);
+  trickle_down(arr, 0, len);
 
   for (int a : arr)
     cout << a << " ";
