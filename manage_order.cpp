22d74cb189d8f3f7115678591cf4e175aa62bbbd
Shirish Bahirat <shirishbahirat@comcast.net>
2021-10-14 14:40:22 -0600

    solved problems

--- manage_order.cpp
+++ manage_order.cpp
@@ -222,7 +222,11 @@ void RedfishConfigHandlerInitilization(void)
   cout << "link list  " << n->next->priority << endl;
   cout << "link list  " << n->next->next->priority << endl;
   cout << "link list  " << n->next->next->next->priority << endl;
-  cout << "link list  " << n->next->next->next->next->priority << endl;
+  // cout << "link list  " << n->next->next->next->next->priority << endl;
+
+  for (Index = 0; Index < NumberOfHandles; Index++)
+  {
+  }
 }
 
 int main(int argc, char const *argv[])
