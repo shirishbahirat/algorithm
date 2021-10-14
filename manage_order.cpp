694681bdde7aa2a1686ed99fe3e991cc6ac1b2bf
Shirish Bahirat <shirishbahirat@comcast.net>
2021-10-14 14:49:02 -0600

    solved problems

--- manage_order.cpp
+++ manage_order.cpp
@@ -224,14 +224,16 @@ void RedfishConfigHandlerInitilization(void)
   cout << "link list  " << n->next->next->next->priority << endl;
   // cout << "link list  " << n->next->next->next->next->priority << endl;
 
-  for (Index = 0; Index < NumberOfHandles - 3; Index++)
-  {
-    driver_priority priority = n->priority;
-    UINTN Index = n->Index;
-    EDKII_REDFISH_CONFIG_HANDLER_PROTOCOL *cfhandler = n->cfhandler;
-    cfhandler->Init();
-    n = n->next;
-  }
+  /*
+    for (Index = 0; Index < NumberOfHandles - 3; Index++)
+    {
+      driver_priority priority = n->priority;
+      UINTN Index = n->Index;
+      EDKII_REDFISH_CONFIG_HANDLER_PROTOCOL *cfhandler = n->cfhandler;
+      cfhandler->Init();
+      n = n->next;
+    }
+    */
 }
 
 int main(int argc, char const *argv[])
