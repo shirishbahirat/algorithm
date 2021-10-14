f6bc90d9e0a88f36bb100674d8f3589dcad0e678
Shirish Bahirat <shirishbahirat@comcast.net>
2021-10-14 16:58:28 -0600

    solved problems

--- manage_order.cpp
+++ manage_order.cpp
@@ -246,7 +246,7 @@ int main(int argc, char const *argv[])
 {
 
   id = 0;
-  driver_priority arr[] = {HIGH, MEDIUM, MEDIUM, MEDIUM, LOW};
+  driver_priority arr[] = {HIGH, HIGH, HIGH, LOW, LOW};
 
   gBs = new (_gBs);
   gBs->LocateHandleBuffer = locateHandleBuffer;
