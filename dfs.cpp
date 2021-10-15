699ed8b0c7754ff1bbe9f4e47ac4d79e40773fd6
Shirish Bahirat <shirishbahirat@comcast.net>
2021-10-15 14:21:44 -0600

    solved problems

--- dfs.cpp
+++ dfs.cpp
@@ -1,30 +1,108 @@
+#include <cstring>
+#include <fstream>
 #include <iostream>
+
+using namespace std;
 
 struct schema
 {
   int level;
-  char *name;
-  char *parent;
+  char name[30];
+  char parent[30];
   schema *next;
+  schema *side;
   bool mapped;
 };
 
-schema *create_node(int level, char *name, char *parent)
+schema *split_line(string str, schema *sch)
 {
-  schema *node = new schema();
-  node->level = level;
-  node->name = name;
-  node->parent = parent;
-  node->next = nullptr;
-  node->mapped = false;
+  string word = "";
+  int idx = 0;
 
-  return node;
+  for (auto x : str)
+  {
+    if (x == ' ')
+    {
+      if (!idx)
+      {
+        sch->level = atoi(word.c_str());
+      }
+      else if (idx > 0)
+      {
+        strcpy(sch->name, word.c_str());
+      }
+      word = "";
+      idx++;
+    }
+    else
+    {
+      word = word + x;
+    }
+  }
+
+  strcpy(sch->parent, word.c_str());
+  sch->next = nullptr;
+  sch->mapped = false;
+  sch->side = nullptr;
+
+  return sch;
 }
 
-int level[] = {};
-char *name[] = {};
-char *parent[] = {"Root", "Boot", "Systems"};
+schema *insert_schema(schema *head, schema *node)
+{
 
-void inser_schema(schema **sch) {}
+  if (head == nullptr)
+  {
+    head = node;
+    cout << endl;
+    return head;
+  }
 
-int main(int argc, char const *argv[]) { return 0; }
+  schema *temp = head;
+
+  while (head->next != nullptr)
+  {
+    head = head->next;
+  }
+
+  head->next = node;
+  head = temp;
+  return head;
+}
+
+schema *create_schema(schema *head)
+{
+
+  schema *sch;
+
+  ifstream file("schema.inf");
+  if (file.is_open())
+  {
+    string line;
+    while (getline(file, line))
+    {
+      sch = new schema();
+      sch = split_line(line.c_str(), sch);
+      head = insert_schema(head, sch);
+    }
+    file.close();
+  }
+
+  return head;
+}
+
+int main(int argc, char const *argv[])
+{
+  schema *head = nullptr;
+
+  head = create_schema(head);
+
+  while (head != nullptr)
+  {
+    cout << "node loop " << head->name << " " << head->level << " "
+         << head->parent << " " << head->mapped << endl;
+    head = head->next;
+  }
+
+  return 0;
+}
