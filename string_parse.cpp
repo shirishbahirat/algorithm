1af3654f50719c3afc9bc2be2bd92bdecef55206
Shirish Bahirat <shirishbahirat@comcast.net>
2021-10-21 21:33:57 -0600

    solved problems

--- string_parse.cpp
+++ string_parse.cpp
@@ -8,6 +8,8 @@ int main(int argc, char const *argv[])
   char name[] = "John Smith";
   char updated[5] = "";
 
+  char *pname = &name[0];
+
   for (int i = 0; i < 4; i++)
   {
     updated[i] = name[i];
