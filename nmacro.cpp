8b950c0c45af2d83d3547eb4723b1a09d0a14129
Shirish Bahirat <shirishbahirat@comcast.net>
2021-10-25 17:21:57 -0600

    solved problems

--- nmacro.cpp
+++ nmacro.cpp
@@ -11,6 +11,10 @@ struct node
 #define ID(version) (NODE_V##version##_CS).data
 #define PID(version) (PNODE_V##version##_CS)->data
 
+char ride[] = "\
+	\"Boot\":\"Disabled\",\
+	\"values\" : [\"once\",\"Disabled\"]";
+
 int main(int argc, char const *argv[])
 {
 
