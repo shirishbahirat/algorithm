59abeb39581ce707db659c797c2de1136308e649
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-02 20:28:27 -0600

    solved problems

--- morror.cpp
+++ morror.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 
-int main(int argc, const char* argv[])
+struct node
 {
+  int data;
+  node *left;
+  node *right;
+};
 
+node *new_node(int data)
+{
+  node *n = new node();
+  n->data = data;
+  n->left = nullptr;
+  n->right = nullptr;
 
-
-
-  return 0;
+  return n;
 }
+
+int main(int argc, const char *argv[]) { return 0; }
