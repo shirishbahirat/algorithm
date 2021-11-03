8d391879d2d35b04ca16134fdd9c3c5f09e7b631
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-02 22:36:26 -0600

    solved problems

---
+++ order.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+  int data;
+  node *left;
+  node *right;
+};
+
+node *new_node(int data)
+{
+  node *n = new node();
+  n->data = data;
+  n->left = nullptr;
+  n->right = nullptr;
+
+  return n;
+}
+
+void in_order(node *n)
+{
+
+  if (n == nullptr)
+    return;
+  in_order(n->left);
+  cout n->data << " ";
+  in_order(n->right);
+}
+
+int main(int argc, const char *argv[])
+{
+
+  node *root = new_node(1);
+  root->left = new_node(2);
+  root->right = new_node(3);
+  root->left->left = new_node(4);
+  root->left->right = new_node(5);
+
+  in_order(root);
+  cout << endl;
+
+  return 0;
+}
