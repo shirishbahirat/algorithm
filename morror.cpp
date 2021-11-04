2b750028cdb35ec86217b04342e8572022228a67
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-03 20:46:22 -0600

    solved problems

--- morror.cpp
+++ morror.cpp
@@ -9,6 +9,17 @@ struct node
   node *right;
 };
 
+int fib(int n)
+{
+  if (n == 0)
+    return 0;
+
+  if (n == 1)
+    return 1;
+
+  return fib(n - 1) + fib(n - 2);
+}
+
 node *new_node(int data)
 {
   node *n = new node();
