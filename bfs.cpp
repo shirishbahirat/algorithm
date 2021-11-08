ab094bf13a97d5ede9a68f318e653b50318058a5
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-07 18:22:57 -0700

    solved problems

--- bfs.cpp
+++ bfs.cpp
@@ -49,8 +49,8 @@ void pre_order(node *n)
     return;
 
   cout << n->data << " ";
-  in_order(n->left);
-  in_order(n->right);
+  pre_order(n->left);
+  pre_order(n->right);
 }
 
 int main(int argc, const char *argv[])
