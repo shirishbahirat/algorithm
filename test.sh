640b494a88a24de115a12e47921622a9bfaf88eb
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-11 22:07:27 -0700

    bash script test

---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+COLOR="Red"
+VALUE=10
+
+PARA1="$1"
+PARA2="$2"
+PARA3="$3"
+
+idx=10
+idy=20
+idz=($idx+$idy)
+
+my_function() {
+	echo "from test function: $COLOR"
+	echo "parameters: $PARA1 $PARA2 $PARA3"
+	echo sp{el,il,al}l
+	echo ~
+	echo ~/
+	echo ~+
+	echo `date`
+	echo $[idx + 100]
+
+	if [[ "$idx" == "11" ]]; then
+		echo "if statement is true";
+	else 
+		echo "if statement is false";		
+	fi
+
+
+}
+
+another_function() {
+	echo "from another function: $VALUE"
+	echo "after incrementing: $idx $idy $idz"
+}
+
+my_function
+another_function
