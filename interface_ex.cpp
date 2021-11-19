7850792bcde69c5092a429e7fc344c92f67d3f6c
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-18 20:21:54 -0700

    solved problems

--- interface_ex.cpp
+++ interface_ex.cpp
@@ -18,9 +18,9 @@ int *device_ready_interface(int a)
 
 int main(int argc, char const *argv[])
 {
-
-  //*x = 20;
-  // interface intr = {.device_ready = device_ready_interface};
+  int m = 20;
+  x = &m;
+  interface intr = {.device_ready = device_ready_interface};
 
   return 0;
 }
