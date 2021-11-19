937e1ff39bec5f08951a2ed851071942fb8a687e
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-18 20:19:48 -0700

    solved problems

---
+++ interface_ex.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct inf
+{
+
+  int *(*device_ready)(int);
+
+} interface;
+
+int *x;
+
+int *device_ready_interface(int a)
+{
+  *x = 10 + a;
+  cout << *x << endl;
+  return x;
+}
+
+int main(int argc, char const *argv[])
+{
+
+  *x = 20;
+  interface intr { .device_ready = device_ready_interface; };
+
+  return 0;
+}
