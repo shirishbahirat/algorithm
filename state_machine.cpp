43ab5f9fb7d9e9ca1b5fe635fe41ae66a9b902f5
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-07 19:48:17 -0700

    solved problems

--- state_machine.cpp
+++ state_machine.cpp
@@ -19,7 +19,7 @@ struct state_machine
   int (*close)(state_t);
 };
 
-int execute_state(state_t name)
+int state_execute(state_t name)
 {
 
   switch (name)
@@ -37,13 +37,49 @@ int execute_state(state_t name)
     state = INIT;
   }
 
+  cout << state << endl;
+
   return 0;
 }
 
 int state_change(state_t name)
 {
   state = name;
+  cout << state << endl;
   return 0;
 }
 
-int main(int argc, char const *argv[]) { return 0; }
+int state_init(state_t name)
+{
+  if (name == INIT)
+  {
+    state = INIT;
+    cout << state << endl;
+    return 0;
+  }
+  return 1;
+}
+
+int state_close(state_t name)
+{
+  if (name == DONE)
+  {
+    state = DONE;
+    cout << state << endl;
+    return 0;
+  }
+  return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+
+  state_machine machine = {.execute = state_execute,
+                           .change = state_change,
+                           .init = state_init,
+                           .close = state_close};
+
+  machine.init(INIT);
+
+  return 0;
+}
