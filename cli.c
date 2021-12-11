51918c6d13aaf23344e4d26a62a9f38063f44fb8
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-10 19:14:56 -0700

    solved problems

--- cli.c
+++ cli.c
@@ -3,4 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
 int main(int argc, char *argv[]) { return 0; }
