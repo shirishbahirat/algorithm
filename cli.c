61f6c74e56222cf0e9c8031992c47340d86aea29
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-10 19:36:07 -0700

    solved problems

--- cli.c
+++ cli.c
@@ -51,7 +51,7 @@ void *cli_task()
   while (1)
   {
     printf(">> ");
-    scanf("%20[^\n]s", data);
+    scanf("%20s", data);
     if (sizeof(data) > 0)
     {
       pthread_mutex_lock(&management.lock);
