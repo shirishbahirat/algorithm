081c40bf1323fa2c8573db0bff6b4ca2d4a1a765
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-11 10:28:44 -0700

    solved problems

--- va_args.cpp
+++ va_args.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 
+#define MMU_DEBUG(...)                                                         \
+  do                                                                           \
+  {                                                                            \
+    printf(__VA_ARGS__);                                                       \
+  } while (0)
+
 int main(int argc, char const *argv[]) { return 0; }
