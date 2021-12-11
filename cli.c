b8a9e65d5b9d8d04a23111f2f6e8ede31f22ba22
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-10 19:20:57 -0700

    solved problems

--- cli.c
+++ cli.c
@@ -12,4 +12,64 @@ typedef struct _mgmet
 mgmet management = {.cond = PTHREAD_COND_INITIALIZER,
                     .lock = PTHREAD_MUTEX_INITIALIZER};
 
-int main(int argc, char *argv[]) { return 0; }
+void execute_command(char *command)
+{
+
+  char cm[] = "shirish";
+
+  if (!strcmp(command, cm))
+  {
+    printf("Set data %s\n", command);
+  }
+
+  return;
+}
+
+char data[20];
+
+void *dispatcher_task()
+{
+
+  while (1)
+  {
+    pthread_mutex_lock(&management.lock);
+    while (1)
+    {
+      pthread_cond_wait(&management.cond, &management.lock);
+    }
+    pthread_cond_signal(&management.cond);
+
+    pthread_mutex_unlock(&management.lock);
+  }
+
+  return NULL;
+}
+
+void *cli_task()
+{
+
+  while (1)
+  {
+    printf(">>");
+    scanf("%19s", data);
+    pthread_mutex_lock(&management.lock);
+    pthread_cond_signal(&management.cond);
+    pthread_mutex_unlock(&management.lock);
+    execute_command(data);
+  }
+
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+
+  pthread_t thread_idx, thread_idy;
+
+  pthread_create(&thread_idx, NULL, dispatcher_task, NULL);
+  pthread_create(&thread_idy, NULL, cli_task, NULL);
+
+  pthread_join(thread_idx, NULL);
+  pthread_join(thread_idy, NULL);
+  return 0;
+}
