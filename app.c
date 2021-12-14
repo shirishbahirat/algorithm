cf7f96859034437e591550ac4e9dd24e2a8c744a
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-13 20:48:47 -0700

    solved problems

--- app.c
+++ app.c
@@ -2,6 +2,28 @@
 
 char data[20];
 
+int vect[] = {10, 3, 5, 3, 9, 11, 6, 8, 0xFFFF};
+
+void swap(int *v, int a, int b)
+{
+  if (a != b)
+  {
+    int temp = v[a];
+    v[a] = v[b];
+    v[b] = temp;
+  }
+}
+
+int partition(int low, int high)
+{
+  int pivot = vect[0];
+
+  int idx = low;
+  int idy = high;
+
+  return 0;
+}
+
 int main(int argc, const char *argv[])
 {
 
