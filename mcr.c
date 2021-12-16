66c450d55ba656291470a47490db7a28f74d89d6
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-15 20:51:12 -0700

    solved problems

---
+++ mcr.c
@@ -0,0 +1,11 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+pthread_cond_t cond;
+pthread_mutex_t lock;
+
+int data = 0;
+
+int main(int argc, char const *argv[]) { return 0; }
