b07a6f99d81b0d97787f1a4157293013e4696df4
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-17 20:11:26 -0700

    solved problems

--- thread_e.c
+++ thread_e.c
@@ -1,4 +1,13 @@
 #include <pthread.h>
 #include <stdio.h>
 
-int main(int argc, const char **argv) { return 0; }
+int main(int argc, const char **argv)
+{
+
+  if (argc > 1)
+  {
+    printf("%s\n", argv[1]);
+  }
+
+  return 0;
+}
