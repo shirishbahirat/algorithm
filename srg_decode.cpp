b2d3e2f87070b7c464f46c8fdc2dcbde97dcc037
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-27 21:46:37 -0700

    solved problems

--- srg_decode.cpp
+++ srg_decode.cpp
@@ -12,6 +12,9 @@ int main(int argc, char **argv)
   char **pparr = &parr;
 
   cout << pparr[0][0] << endl;
+  cout << pparr[0][1] << endl;
+  cout << pparr[0][2] << endl;
+  cout << pparr[0][3] << endl;
 
   cout << argc << endl;
 
