86e3971a54f3efb108428c0e026c75fd63fa2d57
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-03 13:34:48 -0700

    solved problems

---
+++ mloop.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[] = {9, 4, 7, 2, 3, 8, 4};
+
+int sum = 0;
+
+int rsum(int arr[], int idx, int sum)
+{
+
+  sum = sum + arr[idx];
+
+  cout << sum << endl;
+
+  if (idx == 0)
+    return sum;
+  else
+    sum(arr, --idx, sum);
+
+  return 0;
+}
+
+int main(int argc, const char *argv[])
+{
+
+  sum = rsum(arr, 6, sum);
+
+  cout << sum << endl;
+
+  return 0;
+}
