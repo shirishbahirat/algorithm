6df1870f6c52c27b9abc048f66f977cf10ccb228
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-03 13:55:00 -0700

    solved problems

--- mloop.cpp
+++ mloop.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int arr[] = {9, 4, 7, 2, 3, 8, 4};
 
+struc node
+{
+  int data;
+  node *left;
+  node *right;
+};
+
 int sum = 0;
 
 void rsum(int arr[], int idx)
@@ -25,12 +32,22 @@ void rsum(int arr[], int idx)
 
 void lsum(int arr[], int idx)
 {
-
   for (int i = 0; i <= idx; ++i)
   {
     sum += arr[i];
     cout << sum << " " << i << endl;
   }
+}
+
+void btree(int idx)
+{
+
+  int l = 2 * idx + 1;
+  int r = 2 * idx + 2;
+
+  cout << l << " " << r << endl;
+
+  return;
 }
 
 int main(int argc, const char *argv[])
@@ -43,6 +60,10 @@ int main(int argc, const char *argv[])
   sum = 0;
   lsum(arr, 6);
   cout << sum << endl;
+
+  btree(0);
+  btree(1);
+  btree(2);
 
   return 0;
 }
