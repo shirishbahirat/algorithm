a05e4888d869e454d65f75dad332b4b91ccc6206
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-18 22:09:23 -0600

    solved problems

--- lcd.cpp
+++ lcd.cpp
@@ -10,6 +10,39 @@ struct node
   node(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+  TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+  {
+
+    if (root == nullptr)
+      return NULL;
+
+    if (root->val == p->val || root->val == q->val)
+      return root;
+
+    TreeNode *left_lca = lowestCommonAncestor(root->left, p, q);
+    TreeNode *right_lca = lowestCommonAncestor(root->right, p, q);
+
+    if (left_lca == NULL && right_lca == NULL)
+      return NULL;
+    if (left_lca != NULL && right_lca != NULL)
+      return root;
+
+    return (left_lca != NULL) ? left_lca : right_lca;
+  }
+};
+
 void insert(node **hd, int val)
 {
   if ((*hd) == nullptr)
@@ -20,11 +53,11 @@ void insert(node **hd, int val)
 
   if (val <= (*hd)->val)
   {
-    insert(&(*hd)->left, val)
+    insert(&(*hd)->left, val);
   }
   else
   {
-    insert(&(*hd)->right, val)
+    insert(&(*hd)->right, val);
   }
 }
 
