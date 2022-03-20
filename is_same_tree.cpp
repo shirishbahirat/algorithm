c6900030eee59b66831af4ac42c0c1eef47f357a
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-20 13:56:13 -0600

    solved problems

--- is_same_tree.cpp
+++ is_same_tree.cpp
@@ -6,13 +6,36 @@ using namespace std;
 class Solution
 {
 public:
-    int pivotIndex(vector<int> &nums)
-    {
-         return 0;
-    }
+  bool isSameTree(TreeNode *p, TreeNode *q) { return true; }
 };
+
 int main(int argc, char const *argv[])
 {
-    Solution *obj = new Solution();
-    return 0;
-  }
+  Solution *obj = new Solution();
+
+  TreeNode *p = new TreeNode(10);
+
+  p->left = new TreeNode(8);
+  p->right = new TreeNode(14);
+
+  p->left->left = new TreeNode(6);
+  p->left->right = new TreeNode(9);
+
+  p->right->left = new TreeNode(12);
+  p->right->right = new TreeNode(18);
+
+  TreeNode *q = new TreeNode(10);
+
+  q->left = new TreeNode(8);
+  q->right = new TreeNode(14);
+
+  q->left->left = new TreeNode(6);
+  q->left->right = new TreeNode(9);
+
+  q->right->left = new TreeNode(12);
+  q->right->right = new TreeNode(18);
+
+  bool stat = obj->isSameTree(p, q);
+
+  return 0;
+}
