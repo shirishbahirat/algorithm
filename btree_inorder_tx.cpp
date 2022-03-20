7e35cb2e00988589a220d05b8f7acd7b356bb202
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-20 12:50:03 -0600

    solved problems

--- btree_inorder_tx.cpp
+++ btree_inorder_tx.cpp
@@ -30,5 +30,17 @@ public:
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
+
+  TreeNode *head = new TreeNode(10);
+
+  head->left = new TreeNode(8);
+  head->right = new TreeNode(14);
+
+  head->left->left = new TreeNode(6);
+  head->left->right = new TreeNode(9);
+
+  head->right->left = new TreeNode(12);
+  head->right->right = new TreeNode(18);
+
   return 0;
 }
