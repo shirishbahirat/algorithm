f8ca522730ea75f020088b1f79c650545b851b6f
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-21 20:30:35 -0600

    solved problems

--- sorted2bt.cpp
+++ sorted2bt.cpp
@@ -3,16 +3,39 @@
 
 using namespace std;
 
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right)
+  {
+  }
+};
+
 class Solution
 {
 public:
-    int pivotIndex(vector<int> &nums)
-    {
-         return 0;
-    }
+  TreeNode *sortedArrayToBST(vector<int> &nums) {}
 };
+
 int main(int argc, char const *argv[])
 {
-    Solution *obj = new Solution();
-    return 0;
-  }
+  Solution *obj = new Solution();
+
+  TreeNode *head = new TreeNode(10);
+
+  head->left = new TreeNode(8);
+  head->right = new TreeNode(14);
+
+  head->left->left = new TreeNode(6);
+  head->left->right = new TreeNode(9);
+
+  head->right->left = new TreeNode(12);
+  head->right->right = new TreeNode(18);
+
+  return 0;
+}
