b824fd0a074791878343203e7c7dde6adc9620e1
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-22 21:46:47 -0600

    solved problems

--- increasing_order_st.cpp
+++ increasing_order_st.cpp
@@ -20,10 +20,39 @@ class Solution
 {
 public:
   TreeNode *increasingBST(TreeNode *root) { return root; }
+
+  void traversal(TreeNode *root)
+  {
+
+    if (root == nullptr)
+      return;
+
+    traversal(root->left);
+    cout << root->val << endl;
+    traversal(root->right);
+
+    return;
+  }
 };
 
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
+
+  TreeNode *head = new TreeNode(5);
+
+  head->left = new TreeNode(3);
+  head->right = new TreeNode(6);
+
+  head->left->left = new TreeNode(2);
+  head->left->right = new TreeNode(4);
+
+  head->left->left->left = new TreeNode(1);
+
+  head->right->right = new TreeNode(8);
+
+  head->right->right->left = new TreeNode(7);
+  head->right->right->right = new TreeNode(9);
+
   return 0;
 }
