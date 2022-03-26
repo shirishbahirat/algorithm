2a8cc559a999e159c660ebf90eade9c9ae3a67df
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-26 12:42:12 -0600

    solved problems

--- is_unique_value.cpp
+++ is_unique_value.cpp
@@ -19,11 +19,52 @@ struct TreeNode
 class Solution
 {
 public:
-  bool isUnivalTree(TreeNode *root) {}
+  bool result = false;
+  int val;
+
+  void init(TreeNode *root)
+  {
+    if (root != nullptr)
+    {
+      val = root->val;
+    }
+  }
+
+  bool transverse(TreeNode *root)
+  {
+
+    if (root == nullptr)
+      return false;
+
+    transverse(root->left);
+    transverse(root->right);
+    cout << root->val << endl;
+  }
+
+  bool isUnivalTree(TreeNode *root)
+  {
+    init(root);
+
+    return true;
+  }
 };
 
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
+
+  TreeNode *p = new TreeNode(1);
+
+  p->left = new TreeNode(1);
+  p->right = new TreeNode(1);
+
+  p->left->left = new TreeNode(1);
+  p->left->right = new TreeNode(1);
+
+  p->right->left = new TreeNode(1);
+  p->right->right = new TreeNode(1);
+
+  obj->isUnivalTree(p);
+
   return 0;
 }
