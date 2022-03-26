365d1f0f857da9607c336f531c22978e5fdda988
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-26 12:35:50 -0600

    solved problems

--- is_unique_value.cpp
+++ is_unique_value.cpp
@@ -3,16 +3,27 @@
 
 using namespace std;
 
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right)
+  {
+  }
+};
+
 class Solution
 {
 public:
-    int pivotIndex(vector<int> &nums)
-    {
-         return 0;
-    }
+  bool isUnivalTree(TreeNode *root) {}
 };
+
 int main(int argc, char const *argv[])
 {
-    Solution *obj = new Solution();
-    return 0;
-  }
+  Solution *obj = new Solution();
+  return 0;
+}
