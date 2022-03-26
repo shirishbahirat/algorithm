1349b32551105bed4c48b028d90d9b2943d47f23
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-26 17:30:27 -0600

    solved problems

--- merge_two_bin_tree.cpp
+++ merge_two_bin_tree.cpp
@@ -21,7 +21,7 @@ class Solution
 public:
   TreeNode *root;
 
-  void transverse(TreeNode *root1, TreeNode *root2)
+  void transverse(TreeNode *root1, TreeNode *root2, TreeNode *root)
   {
 
     if ((root1 == nullptr) && (root2 == nullptr))
@@ -41,18 +41,61 @@ public:
 
     root = new TreeNode(val);
 
-    transverse(root1->left, root2->left);
-
-    transverse(root1->right, root2->right);
+    if (root1 == nullptr)
+    {
+      transverse(nullptr, root2->left, root->left);
+      transverse(nullptr, root2->right, root->right);
+    }
+    else if (root2 == nullptr)
+    {
+      transverse(root1->left, nullptr, root->left);
+      transverse(root1->right, nullptr, root->right);
+    }
+    else
+    {
+      transverse(root1->left, root2->left, root->left);
+      transverse(root1->right, root2->right, root->right);
+    }
 
     return;
   }
 
-  TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2) { return root; }
+  TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2)
+  {
+
+    transverse(root1, root2, root);
+
+    return root;
+  }
 };
 
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
+
+  TreeNode *p = new TreeNode(1);
+  TreeNode *q = new TreeNode(1);
+
+  p->left = new TreeNode(1);
+  p->right = new TreeNode(1);
+
+  p->left->left = new TreeNode(1);
+  p->left->right = new TreeNode(1);
+
+  p->right->left = new TreeNode(2);
+  p->right->right = new TreeNode(1);
+
+  q->left = new TreeNode(1);
+  q->right = new TreeNode(1);
+
+  q->left->left = new TreeNode(1);
+  q->left->right = new TreeNode(1);
+
+  q->right->left = new TreeNode(2);
+  q->right->right = new TreeNode(1);
+
+  obj->mergeTrees(p, q);
+  ;
+
   return 0;
 }
