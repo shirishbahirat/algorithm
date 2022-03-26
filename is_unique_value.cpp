5749bf899adac2ee63df31c92e2d75df760fb27f
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-26 12:35:05 -0600

    solved problems

---
+++ is_unique_value.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int pivotIndex(vector<int> &nums)
+    {
+         return 0;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution *obj = new Solution();
+    return 0;
+  }
