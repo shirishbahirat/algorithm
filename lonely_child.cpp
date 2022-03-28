0e45584789d0b651f6f45eb95ec57c7af9b7b46c
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-28 16:53:34 -0600

    solved problems

---
+++ lonely_child.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> getLonelyNodes(TreeNode *root)
+  {
+    vector<int> nodes;
+
+    return nodes;
+  }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  return 0;
+}
