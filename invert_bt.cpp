c566f0bc0da389e5b659d301f16dfe7eb6c6bdb2
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-28 16:44:03 -0600

    solved problems

--- invert_bt.cpp
+++ invert_bt.cpp
@@ -3,16 +3,42 @@
 
 using namespace std;
 
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right)
+  {
+  }
+};
+
 class Solution
 {
 public:
-    int pivotIndex(vector<int> &nums)
-    {
-         return 0;
-    }
+  TreeNode *invertTree(TreeNode *root) { return root; }
 };
+
 int main(int argc, char const *argv[])
 {
-    Solution *obj = new Solution();
-    return 0;
-  }
+  Solution *obj = new Solution();
+
+  TreeNode *p = new TreeNode(1);
+  TreeNode *r;
+
+  p->left = new TreeNode(1);
+  p->right = new TreeNode(1);
+
+  p->left->left = new TreeNode(1);
+  p->left->right = new TreeNode(1);
+
+  p->right->left = new TreeNode(2);
+  p->right->right = new TreeNode(1);
+
+  r = obj->invertTree(p);
+
+  return 0;
+}
