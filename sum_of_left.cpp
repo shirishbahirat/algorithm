dd8f22161773d3f3e11b3e6f45273c08079d31eb
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-30 19:44:47 -0600

    solved problems

--- sum_of_left.cpp
+++ sum_of_left.cpp
@@ -3,9 +3,40 @@
 
 using namespace std;
 
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right)
+  {
+  }
+};
+
 class Solution
 {
 public:
+  int sum = 0;
+
+  void transverse(TreeNode *root, bool left)
+  {
+    if (root == nullptr)
+      return;
+
+    if (left)
+    {
+      sum += root->val;
+    }
+
+    transverse(root->left, true);
+    transverse(root->right, false);
+
+    return;
+  }
+
   int sumOfLeftLeaves(TreeNode *root) { return 0; }
 };
 
