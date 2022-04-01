197f9d9e03719148ff6ef00677f7554924f54b77
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-31 18:23:32 -0600

    solved problems

--- bin_tree_path_disp.cpp
+++ bin_tree_path_disp.cpp
@@ -49,14 +49,10 @@ int main(int argc, char const *argv[])
 
   TreeNode *root = new TreeNode(1);
 
-  root->left = new TreeNode(0);
-  root->right = new TreeNode(1);
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(3);
 
-  root->left->left = new TreeNode(0);
-  root->left->right = new TreeNode(1);
-
-  root->right->left = new TreeNode(0);
-  root->right->right = new TreeNode(1);
+  root->left->right = new TreeNode(5);
 
   obj->binaryTreePaths(root);
 
