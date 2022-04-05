47c031eda3df65d5ec18f891c3062aa760231157
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-04 22:16:18 -0600

    solved problems

---
+++ level_order_sum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right)
+  {
+  }
+};
+
+class Solution
+{
+public:
+  vector<double> data;
+  vector<double> averageOfLevels(TreeNode *root) { return data; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  return 0;
+}
