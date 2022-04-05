740a628c8ef1f3fe51222d361fad9078db2adfc6
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-04 22:19:50 -0600

    solved problems

--- level_order_sum.cpp
+++ level_order_sum.cpp
@@ -20,11 +20,38 @@ class Solution
 {
 public:
   vector<double> data;
+
+  void transverse(TreeNode *root, int level)
+  {
+    if (root == nullptr)
+      return;
+
+    transverse(root->left, level + 1);
+    transverse(root->right, level + 1);
+  }
+
   vector<double> averageOfLevels(TreeNode *root) { return data; }
 };
 
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
+
+  TreeNode *root = new TreeNode(3);
+
+  root->left = new TreeNode(5);
+  root->right = new TreeNode(1);
+
+  root->left->left = new TreeNode(6);
+  root->left->right = new TreeNode(2);
+
+  root->right->left = new TreeNode(9);
+  root->right->right = new TreeNode(8);
+
+  root->left->right->left = new TreeNode(7);
+  root->left->right->right = new TreeNode(4);
+
+  obj->averageOfLevels(root);
+
   return 0;
 }
