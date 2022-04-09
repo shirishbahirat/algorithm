50a54eb90270963f48161eae1f83e5a73543f165
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-09 16:37:09 -0600

    solved problems

--- bt_min_depth.cpp
+++ bt_min_depth.cpp
@@ -25,5 +25,18 @@ public:
 int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
+
+  TreeNode *root = new TreeNode(4);
+
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(9);
+
+  root->left->left = new TreeNode(3);
+  root->left->right = new TreeNode(5);
+
+  root->right->right = new TreeNode(7);
+
+  cout << obj->minDepth(root) << endl;
+
   return 0;
 }
