24acfdc7b0509c7cfc3c4545cb34960dc2c39b0f
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-08 20:43:29 -0600

    solved problems

--- find_bt_tild.cpp
+++ find_bt_tild.cpp
@@ -3,16 +3,71 @@
 
 using namespace std;
 
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right)
+  {
+  }
+};
+
 class Solution
 {
 public:
-    int pivotIndex(vector<int> &nums)
+  int tilt = 0;
+
+  int findTilt(TreeNode *root)
+  {
+
+    if (root == nullptr)
+      return;
+
+    int l, r;
+    if (root->left == nullptr)
     {
-         return 0;
+      l = 0
     }
+    else
+    {
+      l = root->left
+    }
+
+    if (root->right == nullptr)
+    {
+      r = 0;
+    }
+    else
+    {
+      r = root->right;
+    }
+
+    tilt += (l - r);
+
+    findTilt(root->left);
+    findTilt(root->right);
+
+    return tilt;
+  }
 };
+
 int main(int argc, char const *argv[])
 {
-    Solution *obj = new Solution();
-    return 0;
-  }
+  Solution *obj = new Solution();
+
+  TreeNode *root = TreeNode(4);
+
+  root->left = TreeNode(2);
+  root->right = TreeNode(9);
+
+  root->left->left = TreeNode(3);
+  root->left->right = TreeNode(5);
+
+  root->right->right = TreeNode(7);
+
+  return 0;
+}
