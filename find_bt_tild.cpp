1622577179e7037a27027983973e4ebf0f1c78b9
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-08 20:58:51 -0600

    solved problems

--- find_bt_tild.cpp
+++ find_bt_tild.cpp
@@ -21,35 +21,24 @@ class Solution
 public:
   int tilt = 0;
 
+  void transverse(TreeNode *root, int tilt)
+  {
+
+    if (!root)
+      return 0;
+
+    int left = transverse(root->left, tilt);
+    int right = transverse(root->right, tilt);
+
+    tilt += abs(left - right);
+
+    return left + right + root->val;
+  }
+
   int findTilt(TreeNode *root)
   {
 
-    if (root == nullptr)
-      return;
-
-    int l, r;
-    if (root->left == nullptr)
-    {
-      l = 0
-    }
-    else
-    {
-      l = root->left
-    }
-
-    if (root->right == nullptr)
-    {
-      r = 0;
-    }
-    else
-    {
-      r = root->right;
-    }
-
-    tilt += (l - r);
-
-    findTilt(root->left);
-    findTilt(root->right);
+    transverse(root, tilt);
 
     return tilt;
   }
@@ -59,15 +48,17 @@ int main(int argc, char const *argv[])
 {
   Solution *obj = new Solution();
 
-  TreeNode *root = TreeNode(4);
+  TreeNode *root = new TreeNode(4);
 
-  root->left = TreeNode(2);
-  root->right = TreeNode(9);
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(9);
 
-  root->left->left = TreeNode(3);
-  root->left->right = TreeNode(5);
+  root->left->left = new TreeNode(3);
+  root->left->right = new TreeNode(5);
 
-  root->right->right = TreeNode(7);
+  root->right->right = new TreeNode(7);
+
+  cout << obj->findTilt(root) << endl;
 
   return 0;
 }
