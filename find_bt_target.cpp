4a861b9113d37eacd19b772ec13be665ca82a407
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-10 08:07:20 -0600

    solved problems

---
+++ find_bt_target.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  bool findTarget(TreeNode *root, int k) { return true; }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  return 0;
+}
