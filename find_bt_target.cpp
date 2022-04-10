71d579bee38f7956bc4e8b03f9be07e3a6df9d0c
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-10 08:09:10 -0600

    solved problems

--- find_bt_target.cpp
+++ find_bt_target.cpp
@@ -3,10 +3,34 @@
 
 using namespace std;
 
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right)
+  {
+  }
+};
+
 class Solution
 {
 public:
-  bool findTarget(TreeNode *root, int k) { return true; }
+  int element;
+
+  bool findTarget(TreeNode *root, int k)
+  {
+
+    if (!root)
+      return false;
+
+    element = k - root->val;
+
+    return true;
+  }
 };
 
 int main(int argc, char const *argv[])
