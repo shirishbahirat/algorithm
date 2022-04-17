9fa06014bfabb2e5fd7f5ba894b540bc4986312e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-17 12:47:33 -0600

    solved problems

--- is_bt_balanced.cpp
+++ is_bt_balanced.cpp
@@ -65,6 +65,9 @@ int main(int argc, char const *argv[])
   root->left->left = new TreeNode(4);
   root->left->right = new TreeNode(7);
 
+  root->right->left = new TreeNode(3);
+  root->right->right = new TreeNode(9);
+
   obj->isBalanced(root);
   return 0;
 }
