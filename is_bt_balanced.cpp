4094eb37eae9035b4cf97e6b0552acc976b3e52c
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-17 11:55:26 -0600

    solved problems

--- is_bt_balanced.cpp
+++ is_bt_balanced.cpp
@@ -3,16 +3,36 @@
 
 using namespace std;
 
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right)
+  {
+  }
+};
 class Solution
 {
 public:
-    int pivotIndex(vector<int> &nums)
-    {
-         return 0;
-    }
+  bool isBalanced(TreeNode *root) { return true; }
 };
+
 int main(int argc, char const *argv[])
 {
-    Solution *obj = new Solution();
-    return 0;
-  }
+  Solution *obj = new Solution();
+
+  TreeNode *root = new TreeNode(5);
+
+  root->left = new TreeNode(6);
+  root->right = new TreeNode(8);
+
+  root->left->left = new TreeNode(4);
+  root->left->right = new TreeNode(7);
+
+  obj->isBalanced(root);
+  return 0;
+}
